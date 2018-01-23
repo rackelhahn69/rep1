593ffd75f75f8fdeb9f585ba42a16852672213f3
ellocke <elias.fubit@gmail.com>
2018-01-23 23:25:18 +0100

    Saving

---
+++ RSnippets/pipeT.R
@@ -0,0 +1,6 @@
+crps <-
+  rawd %>% 
+  corpus(text_field="artc") %T>% 
+  {(clct <- textstat_collocations(., min_count=100))} %>% 
+  tokens(what="word") %>% 
+  tokens_compound(clct, concatenator="_")
