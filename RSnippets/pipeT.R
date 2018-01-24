69f51abb72860e32f052442ddcd7acdd5d03b1be
rackelhahn69 <31383702+rackelhahn69@users.noreply.github.com>
2018-01-24 09:53:35 +0100

    Update pipeT.R

--- RSnippets/pipeT.R
+++ RSnippets/pipeT.R
@@ -1,6 +1,8 @@
 crps <-
   rawd %>% 
   corpus(text_field="artc") %T>% 
+  # geschweifte klammer für den punkt,
+  # glatte klammer für das assignment
   {(clct <- textstat_collocations(., min_count=100))} %>% 
   tokens(what="word") %>% 
   tokens_compound(clct, concatenator="_")
